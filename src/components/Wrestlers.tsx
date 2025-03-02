c70516310a6b93d7519e1e0a931600667fa3e35c
Sahil Udar <udarsahil0@gmail.com>
2025-03-02 17:22:39 +0530

    wrestlers are now visible

--- src/components/Wrestlers.tsx
+++ src/components/Wrestlers.tsx
@@ -1,14 +1,39 @@
 import React, { useEffect, useState } from "react";
 
+interface WrestlerObjectType {
+  "Made by": string; 
+  Source: string;
+  data: {
+    roster: RosterObject;
+  };
+}
 
-const Wrestlers = ({ wrestlerObject }) => {
-  const [wrestlerData, setWrestlerData] = useState(null);
+interface Wrestler {
+  name: string;
+  real_name: string;
+  height: string;
+  nickname: string;
+  promotion: string;
+  debut_year: number;
+  finishers: string[];
+  titles: {
+    current: string[];
+    former: string[];
+  };
+}
+interface RosterObject {
+  [key: string]: Wrestler; 
+}
+
+
+const Wrestlers = ({ wrestlerObject }: { wrestlerObject:WrestlerObjectType}) => {
+  const [wrestlerData, setWrestlerData] = useState<RosterObject | null>(null);
 
   useEffect(() => {
     if (wrestlerObject.data) {
       setWrestlerData(wrestlerObject.data.roster);
-      console.log(wrestlerData);
     }
+
     
   }, [wrestlerObject]);
   
@@ -17,7 +42,13 @@ const Wrestlers = ({ wrestlerObject }) => {
 
   return(
         <div>
-           
+           {wrestlerData && Object.entries(wrestlerData).map(([Id, wrestler]) => (
+            <div key={Id} className="wrestler-card">
+            <h2>{wrestler.name}</h2>
+            <p>Real Name: {wrestler.real_name}</p>
+            <p>Height: {wrestler.height}</p>
+          </div>
+           ))}
         </div>
     );
 };
